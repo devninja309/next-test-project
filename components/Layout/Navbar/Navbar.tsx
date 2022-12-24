308de900679c083c3d309125fa1dc2e64420aa87
luckyboy125 <102401966+devninja309@users.noreply.github.com>
2022-12-24 17:29:31 +0200

    Layout
    Added layout component structure like Header, Footer

---
+++ components/Layout/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import styles from "../../../styles/Navbar.module.css";
+export default function Navbar() {
+  return (
+    <div className={styles.root}>
+      <div>Home</div>
+      <div>About Us</div>
+      <div>Products</div>
+    </div>
+  );
+}
